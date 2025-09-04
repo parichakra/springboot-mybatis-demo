015ee7f7e1464af9768914e3f2cc283c7474b9cc
parichakra <khatrivikram005@gmail.com>
2025-09-04 23:12:50 +0545

    Mapper of both AppUser and Employee created

---
+++ src/main/java/com/parichakra/mybatis/mapper/EmployeeMapper.java
@@ -0,0 +1,16 @@
+package com.parichakra.mybatis.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.parichakra.mybatis.model.Employee;
+
+@Mapper
+public interface EmployeeMapper {
+    List<Employee> findAll();
+    Employee findById(Long id);
+    void insert(Employee employee);
+    void update(Employee employee);
+    void delete(Long id);
+}

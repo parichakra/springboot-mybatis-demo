fa33d8550acbc0193411b2b1d9da49f267b862c6
parichakra <khatrivikram005@gmail.com>
2025-09-04 23:11:55 +0545

    refactor : resource files modified

--- src/main/java/com/parichakra/mybatis/SpringbootMybatisDemoApplication.java
+++ src/main/java/com/parichakra/mybatis/SpringbootMybatisDemoApplication.java
@@ -1,9 +1,11 @@
 package com.parichakra.mybatis;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.parichakra.mybatis.mapper")
 public class SpringbootMybatisDemoApplication {
 
 	public static void main(String[] args) {
